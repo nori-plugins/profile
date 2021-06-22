172a346b769a878ddd93fc7b035d4622e63de145
bruteforce1414 <bruteforce1414@gmail.com>
2021-06-22 13:15:26 +0500

    feat: stub for middleware

--- go.mod
+++ go.mod
@@ -7,7 +7,8 @@ require (
 	github.com/google/wire v0.5.0
 	github.com/nori-io/common/v5 v5.0.0
 	github.com/nori-io/interfaces/database/gorm v1.21.10
-	github.com/nori-io/interfaces/nori/http v1.0.0
+	github.com/nori-io/interfaces/nori/http v1.0.1-0.20210622071626-f87408362907
+	github.com/nori-plugins/http v0.0.0-20210622075315-6f4ebe502f7f // indirect
 	github.com/pkg/errors v0.9.1
 	gorm.io/gorm v1.21.10
 )
--- internal/handler/http/constructor.go
+++ internal/handler/http/constructor.go
@@ -1,18 +1,22 @@
 package http
 
 import (
+	"io"
+
 	"github.com/nori-plugins/profile/internal/handler/http/profile"
+
+	netHttp "net/http"
 
 	"github.com/nori-io/interfaces/nori/http"
 )
 
 type Handler struct {
-	R              http.Http
+	R              http.Router
 	ProfileHandler *profile.Handler
 }
 
 type Params struct {
-	R              http.Http
+	R              http.Router
 	ProfileHandler *profile.Handler
 }
 
@@ -22,8 +26,25 @@ func New(params Params) *Handler {
 		ProfileHandler: params.ProfileHandler,
 	}
 
+	handler.R.Route("/profiles", func(r http.Router) {
+		r.Post("/", handler.ProfileHandler.PostProfile) // POST /profile
+
+		// Subrouters:
+		r.Route("/{id}", func(r http.Router) {
+			r.Use(StubCtx)
+			r.Get("/", handler.ProfileHandler.GetProfileById) // GET /profile/123
+		})
+	})
+
 	// todo: add middleware
-	handler.R.Get("/profile.go/{id}", handler.ProfileHandler.GetProfileById)
-	//handler.R.Post("/auth/signin", handler.ProfileHandler.PostProfile)
+	//handler.R.Get("/profiles/{id}", handler.ProfileHandler.GetProfileById)
+	//handler.R.Post("/profiles", handler.ProfileHandler.PostProfile)
 	return &handler
+}
+
+func StubCtx(next netHttp.Handler) netHttp.Handler {
+	return netHttp.HandlerFunc(func(w netHttp.ResponseWriter, r *netHttp.Request) {
+		io.WriteString(w, "Use")
+		next.ServeHTTP(w, r)
+	})
 }
